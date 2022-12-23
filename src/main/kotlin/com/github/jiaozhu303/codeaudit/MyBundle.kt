9245ab579a6f8ca6ff3955d668da1773ac85adbd
GitHub Action <action@github.com>
2022-12-23 15:40:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/jiaozhu303/codeaudit/MyBundle.kt
+++ src/main/kotlin/com/github/jiaozhu303/codeaudit/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jiaozhu303.codeaudit
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/jiaozhu303/codeaudit/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/jiaozhu303/codeaudit/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.jiaozhu303.codeaudit.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.jiaozhu303.codeaudit.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
