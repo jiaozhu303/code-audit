ee57abb09886d4104049ffdd666a4115f52b17eb
zhaodj5 <zhaodj5@lenovo.com>
2023-12-23 22:29:14 +0800

    update notification management

--- src/main/java/com/dj/tool/ui/CodeAuditNotifier.java
+++ src/main/java/com/dj/tool/ui/CodeAuditNotifier.java
@@ -7,21 +7,21 @@ import com.intellij.openapi.project.Project;
 public class CodeAuditNotifier {
     public static void notifyError(Project project, String content) {
         NotificationGroupManager.getInstance()
-                .getNotificationGroup("Code Audit Notification Group")
+                .getNotificationGroup("Code Audit Notification")
                 .createNotification(content, NotificationType.ERROR)
                 .notify(project);
     }
 
     public static void notifyWarning(Project project, String content) {
         NotificationGroupManager.getInstance()
-                .getNotificationGroup("Code Audit Notification Group")
+                .getNotificationGroup("Code Audit Notification")
                 .createNotification(content, NotificationType.WARNING)
                 .notify(project);
     }
 
     public static void notifyInfo(Project project, String content) {
         NotificationGroupManager.getInstance()
-                .getNotificationGroup("Code Audit Notification Group")
+                .getNotificationGroup("Code Audit Notification")
                 .createNotification(content, NotificationType.INFORMATION)
                 .notify(project);
     }
