d30669dee1544bba0f0e79f781e82367d098be67
Dongjie <zhaodj8@lenovo.com>
2024-01-03 20:03:08 +0800

    refactor project with kotlin

--- src/main/kotlin/com/dj/tool/action/CodeAuditSetting.kt
+++ src/main/kotlin/com/dj/tool/action/CodeAuditSetting.kt
@@ -2,7 +2,6 @@ package com.dj.tool.action
 
 import com.dj.tool.common.ApplicationCache
 import com.dj.tool.ui.CodeAuditSettingDialog
-import com.dj.tool.ui.CodeAuditSettingDialogVersion2
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
@@ -10,8 +9,10 @@ import com.intellij.openapi.actionSystem.CommonDataKeys
 class CodeAuditSetting : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val model = ApplicationCache.codeAuditSetting
-        CodeAuditSettingDialog.showDialog(model)
-//        if (CodeAuditSettingDialogVersion2(e.getData(CommonDataKeys.PROJECT), model).showAndGet()) {
-//        }
+//        CodeAuditSettingDialog.showDialog(model)
+        val codeAuditSettingDialog = CodeAuditSettingDialog(e.getData(CommonDataKeys.PROJECT), model)
+        if (codeAuditSettingDialog.showAndGet()) {
+            codeAuditSettingDialog.save()
+        }
     }
 }
--- src/main/kotlin/com/dj/tool/ui/CodeAuditSettingDialog.kt
+++ src/main/kotlin/com/dj/tool/ui/CodeAuditSettingDialog.kt
@@ -2,37 +2,38 @@ package com.dj.tool.ui
 
 import com.dj.tool.common.ApplicationCache
 import com.dj.tool.model.CodeAuditSettingModel
-import java.awt.Dimension
-import java.awt.Toolkit
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.DialogWrapper
+import com.sun.java.accessibility.util.AWTEventMonitor.addWindowListener
+import java.awt.BorderLayout
 import java.awt.event.KeyEvent
 import java.awt.event.WindowAdapter
 import java.awt.event.WindowEvent
-import javax.swing.*
+import javax.swing.JComponent
+import javax.swing.JPanel
+import javax.swing.JTextField
+import javax.swing.KeyStroke
 
-class CodeAuditSettingDialog : JDialog() {
+class CodeAuditSettingDialog(project: Project?, private val model: CodeAuditSettingModel) :
+    DialogWrapper(project, true) {
+
     private var contentPane: JPanel? = null
-    private var buttonOK: JButton? = null
-    private var buttonCancel: JButton? = null
     private var urlTextField: JTextField? = null
     private var userNameTextField: JTextField? = null
     private var passwordTextField: JTextField? = null
     private var spaceKeyTextField: JTextField? = null
     private var parentIdTextField: JTextField? = null
 
-
     init {
-        isModal = true
-        setContentPane(contentPane)
+        init()
         title = "Code Audit Setting"
-        minimumSize = Dimension(600, 200)
-        getRootPane().defaultButton = buttonOK
-
-        buttonOK!!.addActionListener { onOK() }
-
-        buttonCancel!!.addActionListener { onCancel() }
+        urlTextField!!.text = model.url
+        userNameTextField!!.text = model.userName
+        passwordTextField!!.text = model.password
+        spaceKeyTextField!!.text = model.spaceKey
+        parentIdTextField!!.text = model.parentId
 
         // call onCancel() when cross is clicked
-        defaultCloseOperation = DO_NOTHING_ON_CLOSE
         addWindowListener(object : WindowAdapter() {
             override fun windowClosing(e: WindowEvent) {
                 onCancel()
@@ -45,9 +46,21 @@ class CodeAuditSettingDialog : JDialog() {
             KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
             JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT
         )
+        setSize(600, 300)
     }
 
-    private fun onOK() {
+    private fun onCancel() {
+        // add your code here if necessary
+        dispose()
+    }
+
+    override fun createCenterPanel(): JComponent? {
+        var dialogPanel = JPanel(BorderLayout())
+        dialogPanel.add(contentPane, BorderLayout.CENTER)
+        return dialogPanel;
+    }
+
+    fun save() {
         ApplicationCache.saveCodeAuditSetting(
             urlTextField!!.text,
             userNameTextField!!.text,
@@ -58,29 +71,4 @@ class CodeAuditSettingDialog : JDialog() {
         dispose()
     }
 
-    private fun onCancel() {
-        // add your code here if necessary
-        dispose()
-    }
-
-    companion object {
-        private const val WIDTH = 600
-        private const val HEIGHT = 600
-
-        fun showDialog(model: CodeAuditSettingModel) {
-            val dialog = CodeAuditSettingDialog()
-            dialog.urlTextField!!.text = model.url
-            dialog.userNameTextField!!.text = model.userName
-            dialog.passwordTextField!!.text = model.password
-            dialog.spaceKeyTextField!!.text = model.spaceKey
-            dialog.parentIdTextField!!.text = model.parentId
-
-            val screenSize = Toolkit.getDefaultToolkit().screenSize
-            val w = (screenSize.width - WIDTH) / 2
-            val h = (screenSize.height * 95 / 100 - HEIGHT) / 2
-            dialog.setLocation(w, h)
-            dialog.pack()
-            dialog.isVisible = true
-        }
-    }
 }
