5520cad28b81a6d60f11cbb2a199e75e290547bc
Dongjie <zhaodj8@lenovo.com>
2024-01-04 20:01:45 +0800

    - remove unused code

--- src/main/java/com/dj/tool/provider/MarkIconProviderFactory.java
+++ src/main/java/com/dj/tool/provider/MarkIconProviderFactory.java
@@ -1,21 +1,17 @@
 package com.dj.tool.provider;
 
-import com.dj.tool.action.LeftMarkIconProvider;
-import com.intellij.openapi.components.ServiceManager;
-import com.intellij.openapi.project.Project;
-
 public class MarkIconProviderFactory {
-
-    public static LeftMarkIconProvider getInstance(Project project) {
-//        MyApplicationService applicationService = ServiceManager.getService(MyApplicationService.class);
-//        MyProjectService projectService = ServiceManager.getService(project, MyProjectService.class);
-//        MyModuleService moduleService = ModuleServiceManager.getService(module, MyModuleService.class);
-//        return ApplicationManager.getApplication().getService(DateRefreshMessagePublisher.class);
-//        return project.getActualComponentManager().getService(DateRefreshMessagePublisher.class);
-
-        LeftMarkIconProvider provider = ServiceManager.getService(project, LeftMarkIconProvider.class);
-        return provider;
-    }
+//
+//    public static LeftMarkIconProvider getInstance(Project project) {
+////        MyApplicationService applicationService = ServiceManager.getService(MyApplicationService.class);
+////        MyProjectService projectService = ServiceManager.getService(project, MyProjectService.class);
+////        MyModuleService moduleService = ModuleServiceManager.getService(module, MyModuleService.class);
+////        return ApplicationManager.getApplication().getService(DateRefreshMessagePublisher.class);
+////        return project.getActualComponentManager().getService(DateRefreshMessagePublisher.class);
+//
+//        LeftMarkIconProvider provider = ServiceManager.getService(project, LeftMarkIconProvider.class);
+//        return provider;
+//    }
 
 
 }
